79f5b1d093c65440ee0d5164de7eedd47b8aeb43
Torstein Nicolaysen <torstein.nicolaysen@vipps.no>
2018-09-26 08:54:57 +0200

    force ascii encoding when generating JSON

--- yaml2json.py
+++ yaml2json.py
@@ -7,5 +7,5 @@ ipp_yaml = yaml.safe_load(open("docs/swagger-ipp.yaml", "r", encoding="utf-8"))
 ipp_json = open("docs/swagger-ipp.json", "w", encoding="utf-8")
 isp_json = open("docs/swagger-isp.json", "w", encoding="utf-8")
 
-ipp_json.write(json.dumps(ipp_yaml, indent=2))
-isp_json.write(json.dumps(isp_yaml, indent=2))
+ipp_json.write(json.dumps(ipp_yaml, indent=2, ensure_ascii=False))
+isp_json.write(json.dumps(isp_yaml, indent=2, ensure_ascii=False))
